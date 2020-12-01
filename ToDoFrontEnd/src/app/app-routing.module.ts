737d560fc95d0e5824fade2a185a5088c79c1503
Ge Luxue <lge5@slb.com>
2020-12-01 11:22:48 +0800

    add router for navigation

--- ToDoFrontEnd/src/app/app-routing.module.ts
+++ ToDoFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ListTodoitemComponent },
+  { path: "create", component: CreateTodoitemComponent },
+  { path: "detail", component: TodoitemDetailComponent },
+  { path: "edit", component: UpdateTodoItemComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
